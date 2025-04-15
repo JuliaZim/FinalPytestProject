299d64d5f4c352118ae745f731df42d330d8be6b
Julia Zimina <krypina2009@yandex.ru>
2025-04-15 17:24:16 +0300

    change test to atom

--- Test/conftest.py
+++ Test/conftest.py
@@ -5,6 +5,7 @@ import time
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import FirefoxProfile
 
+
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default=None,
                      help="Choose browser: chrome or firefox")
@@ -13,9 +14,11 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="function")
 def browser(request):
+    link = "http://selenium1py.pythonanywhere.com/"
     browser_name = request.config.getoption("browser_name")
     language = request.config.getoption("language")
     browser = None
+    link = "http://selenium1py.pythonanywhere.com/"
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         browser = webdriver.Chrome()
